4c892028ba6f946f9f68047a163798a53b507591
Victor Huang <vichuang1997@gmail.com>
2017-12-08 15:23:41 +0800

    HW4 preproc bug fixed

--- hw4/preproc.py
+++ hw4/preproc.py
@@ -11,13 +11,13 @@ def preprocessTestingData(path):
     w2v_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model/word2vec.pkl')
     cmap = loadPreprocessCmap(cmap_path)
     transformByConversionMap(lines, cmap)
+    
     lines = padLines(lines, '_', maxlen)
-
     w2v = Word2Vec.load(w2v_path)
     transformByWord2Vec(lines, w2v)
     return lines
 
-def preprocessTrainingData(label_path, nolabel_path='', retrain=False, punctuation=True):
+def preprocessTrainingData(label_path, nolabel_path, retrain=False, punctuation=True):
     print('Loading training data...')
     if retrain:
         preprocess(label_path, nolabel_path)
@@ -27,11 +27,11 @@ def preprocessTrainingData(label_path, nolabel_path='', retrain=False, punctuati
     cmap_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model/cmap.pkl')
     w2v_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model/word2vec.pkl')
     lines = readData(corpus_path, label=False)[:len(lines)]
+    shuffleData(lines, labels)
     labels = np.array(labels)
 
     cmap = loadPreprocessCmap(cmap_path)
     transformByConversionMap(lines, cmap)
-
     if not punctuation:
         removePunctuations(lines)
 
