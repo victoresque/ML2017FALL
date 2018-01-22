17f1012e2280b31f726bec4e07b84218640e3069
Victor Huang <vichuang1997@gmail.com>
2018-01-22 09:04:42 +0800

    HW6 remove unnecessary files

--- hw6/aevis.py
+++ hw6/aevis.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 plt.rcParams["figure.figsize"] = [8, 6]
 '''
 image = np.load('data/visualization.npy').astype(np.float32)
@@ -22,26 +21,27 @@ x = Dense(128, activation='selu')(x)
 output = Dense(28*28, activation='selu')(x)
 
 encoder = Model(input, encoded)
-encoder.load_weights('ae32.h5', by_name=True)
+encoder.load_weights('data/ae32.h5', by_name=True)
 
 X = encoder.predict(image, batch_size=256, verbose=1)
 np.save('data/aevis.npy', X)
+
+
+X = np.load('data/aevis.npy')
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=20, verbose=1).fit(X)
+label = kmeans.predict(X)
+np.save('data/aevis_kmeans.npy', label)
+
+from sklearn.manifold import TSNE
+X = TSNE(n_components=2, n_iter=1000, verbose=1).fit_transform(X)
+np.save('data/aevis_tsne.npy', X)
+del X
 '''
-
-# X = np.load('data/aevis.npy')
-# from sklearn.cluster import KMeans
-# kmeans = KMeans(n_clusters=20, verbose=1).fit(X)
-# label = kmeans.predict(X)
-# np.save('data/aevis_kmeans.npy', label)
-
-# from sklearn.manifold import TSNE
-# X = TSNE(n_components=2, n_iter=1000, verbose=1).fit_transform(X)
-# np.save('data/aevis_tsne.npy', X)
-
 X = np.load('data/aevis_tsne.npy')
 
-cmap0 = matplotlib.cm.get_cmap('YlGnBu')
-cmap1 = matplotlib.cm.get_cmap('YlOrRd')
+cmap0 = matplotlib.cm.get_cmap('Blues')
+cmap1 = matplotlib.cm.get_cmap('Oranges')
 
 ax =  plt.subplots()[1]
 ax.scatter(X[5000:, 0], X[5000:, 1], marker='.', color=cmap0(0.5))
